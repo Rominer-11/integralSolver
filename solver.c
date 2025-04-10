d34939ff66ed4ff514d8a160aaefb833b3cb0d0a
rominer_11 <augustcho2000@gmail.com>
2025-04-09 19:31:38 -0700

    input

---
+++ solver.c
@@ -0,0 +1,18 @@
+/**
+ * Todo...
+ * 1. Figure out how to input equations
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	char equation[128];
+
+	printf("y = ");
+	fgets(equation, 64, stdin);
+
+	return 0;
+}
+
