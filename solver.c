bba04312259f143a9d97954656f4d2819d5b6a70
rominer_11 <augustcho2000@gmail.com>
2025-04-21 18:16:55 -0700

    Made parenthesis finding function

--- solver.c
+++ solver.c
@@ -4,14 +4,34 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+
+/**
+ * Finds the position of the parenthesis that starts the next expression to be evaluated according to PEMDAS
+ */
+int nextParenthesis(char * equation)
+{
+	int i = 0;
+	while (equation[i] != ')' && i < strlen(equation))
+	{
+		++i;
+	}
+	while (equation[i] != '(' && i >= 0)
+	{
+		--i;
+	}
+	return i;
+}
 
 int main()
 {
-	int x;
+	int x, i;
 	char equation[128];
 
 	printf("y = ");
 	fgets(equation, 64, stdin);
+
+	printf("%i", nextParenthesis(equation));
 
 	return 0;
 }
