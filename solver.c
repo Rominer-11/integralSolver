8539c396639a481da809fd96961ac524135d2c71
rominer_11 <augustcho2000@gmail.com>
2025-04-22 21:45:45 -0700

    Added whitespace remover

--- solver.c
+++ solver.c
@@ -25,12 +25,27 @@ int nextParenthesis(char * equation)
 
 int main()
 {
-	int x, i;
 	char equation[128];
-
 	printf("y = ");
 	fgets(equation, 64, stdin);
 
+	/**
+	 * Remove spaces
+	 */
+	char tempEquation[128];
+	int i;
+	int j = 0;
+	for (i = 0; i < strlen(equation); ++i)
+	{
+		if (equation[i] != ' ')
+		{
+			tempEquation[j] = equation[i];
+			++j;
+		}
+	}
+	strcpy(equation, tempEquation);
+
+	printf("%s", equation);
 	printf("%i", nextParenthesis(equation));
 
 	return 0;
